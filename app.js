c7677293c6fefab8faf55824cfba2ec151df0555
W-McPhail <william.m.mcphail@gmail.com>
2022-07-05 21:50:25 -0400

    changes
    ^

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 
-//getComputedStyle(willowTree = "hey.")
-cookieParser(console.log("All good."));
 app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -28,17 +21,5 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 
 module.exports = app;
